5c9c9d7a0f1fdc8ddbb8c21e83e3ce0251051028
Simon McBride <rexnash@gmail.com>
2019-01-10 05:53:44 +0000

    Day three homework

---
+++ exercise_a.rb
@@ -0,0 +1,50 @@
+#Given the following data structure:
+
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+
+# Complete these tasks:
+
+# Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+
+# Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen Street")
+
+# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+
+# Work out the index position of "Linlithgow"
+p "Linlithgow is located at index postion: #{stops.index("Linlithgow")}."
+
+# Remove "Livingston" from the array using its name
+stops.delete("Livingston")
+
+# Delete "Cumbernauld" from the array by index
+stops.delete_at(6) # Delete from index pos 4 if not commented out
+
+# How many stops there are in the array?
+p "There are currently #{stops.count} stops in the array."
+
+# How many ways can we return "Falkirk High" from the array?
+puts # Print a blank line
+p "Return Falkirk High..."
+p "By index pos: #{stops[3]}"
+p "By negative index pos: #{stops[-4]}"
+p "By using the .at() method: #{stops.at(3)}"
+p "Is Falkirk High in the array true/false?: #{stops.include?("Falkirk High")}"
+######result1 = find_chicken_by_name(chicken_hashes, "Audrey")
+
+# Reverse the positions of the stops in the array
+puts
+p "Here is the reversed array..."
+for stop in stops.reverse
+  p stop
+end
+puts
+
+# Print out all the stops using a for loop
+p "Print out all the stops..."
+for stop in stops
+  p stop
+end
---
+++ exercise_b.rb
@@ -0,0 +1,89 @@
+# Given the following data structure:
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+
+# Complete these tasks:
+
+# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
+p "Jonathans twitter handle is: #{users["Jonathan"][:twitter]}."
+
+# Get Erik's hometown
+p "Erik's hometown is: #{users["Erik"][:home_town]}."
+
+# Get the array of Erik's lottery numbers
+p "Erik's lottery numbers are: #{users["Erik"][:lottery_numbers].sort}"
+
+# Get the type of Avril's pet Monty
+p "Avril's pet Monty is a: #{users["Avril"][:pets]}."
+
+# Get the smallest of Erik's lottery numbers
+p "Erik's smallest lottery number is: #{users["Erik"][:lottery_numbers].sort.first}"
+
+# Return an array of Avril's lottery numbers that are even
+avril_lottery_even = users["Avril"][:lottery_numbers]
+p "Avril's even lottery numbers are: #{avril_lottery_even.sort.select(&:even?)}"
+
+# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+p "Add 7 to Erik's lottery numbers: #{users["Erik"][:lottery_numbers].unshift(7)}"
+
+# Change Erik's hometown to Edinburgh
+p "Erik's hometown was changed to: #{users["Erik"][:home_town] = "Edinburgh"}"
+
+# Add a pet dog to Erik called "Fluffy"
+
+
+# Add another person to the users hash
